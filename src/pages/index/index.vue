a38b248a8edcad3e66522121527ae3f77299bc5a
huyuelong <949910047@qq.com>
2024-06-19 00:06:01 +0800

    轮播图指示点

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-//
+import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
-  <uni-card
-    title="基础卡片"
-    sub-title="副标题"
-    extra="额外信息"
-    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-  >
-    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-  </uni-card>
+  <!-- 自定义导航栏 -->
+  <CustomNavbar />
+  <!-- 自定义轮播图 -->
+  <XtxSwiper />
   <view class="index">index</view>
 </template>
 
