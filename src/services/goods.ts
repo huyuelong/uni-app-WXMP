8a113d2622ee1a6f83717afcdfdac15514129786
huyuelong <949910047@qq.com>
2024-06-25 18:30:24 +0800

    商品详情的准备工作

---
+++ src/services/goods.ts
@@ -0,0 +1,15 @@
+import { http } from '@/utils/http'
+
+/**
+ * 商品详情
+ * @param id 商品id
+ */
+export const getGoodsByIdAPI = (id: string) => {
+  return http({
+    method: 'GET',
+    url: '/goods',
+    data: {
+      id,
+    },
+  })
+}
