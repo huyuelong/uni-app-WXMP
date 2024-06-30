e81e8fc254e703013c822a3ffba707ebb751c790
huyuelong <949910047@qq.com>
2024-06-30 18:40:59 +0800

    SKU模块-商品信息渲染

--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,12 +1,6 @@
-/**
- * declare module '@vue/runtime-core'
- *   现调整为
- * declare module 'vue'
- */
 import XtxSwiper from '@/components/XtxSwiper.vue'
 import XtxGuess from '@/components/XtxGuess.vue'
 
-import 'vue'
 declare module 'vue' {
   export interface GlobalComponents {
     XtxSwiper: typeof XtxSwiper
@@ -16,3 +10,4 @@ declare module 'vue' {
 
 // 组件实例类型
 export type XtxGuessInstance = InstanceType<typeof XtxGuess>
+export type XtxSwiperInstance = InstanceType<typeof XtxSwiper>
